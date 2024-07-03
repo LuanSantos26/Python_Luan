4e1ed89bcc0d4742326c0273ec795dbb10014f6b
Luan Feitosa Santos <luansantos26@outlook.com>
2024-07-03 20:16:55 -0300

    .

---
+++ 001.py
@@ -0,0 +1,18 @@
+print('ola mundo')
+"""======================
+    isto é uma string multi lines,usada como bloco de comentarios      
+====================="""
+
+a="Luan"
+b=5.3
+c="teste"
+
+print(a)
+print(b)
+print(type(a))
+print(type(b))
+
+a=str(3)
+b=float(3)
+c=int(3)
+#python é sensiteve case, ela diferencia a minuscula de maiuscula.
