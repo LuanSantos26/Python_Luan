493e4747989895fd56826d0d27bdc9c97c19e13b
Luan Feitosa Santos <luansantos26@outlook.com>
2024-07-03 21:46:03 -0300

    Update 002.py

--- 002.py
+++ 002.py
@@ -2,7 +2,11 @@ x = ("incrivel")
  
 def myfunc():
     x ="fantatico"
+    global z #transofrma variavel local em global
+    z="Bem vindo ao python"
+    
     print("python é "  + x) #dentro da função o x vira uma variavel local, fora da função ela é global e pode ser usada em qualquer lugar dentro do codigo.
 myfunc()
 
 print("Você é " + x )
+print(z)
