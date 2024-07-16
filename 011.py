38f888328b7c9fef1cfa262ae2bf550c0165b1ac
Luan Feitosa Santos <luansantos26@outlook.com>
2024-07-16 19:44:04 -0300

    Update 011.py

--- 011.py
+++ 011.py
@@ -1,17 +1,23 @@
-"""
-Operadores de comparação (relacionais)
-OP      Significado         Exemplo (True)
->       maior               2 > 1
->=      maior ou igual      2 >= 2
-<       menor               1 < 2
-<=      menor ou igual      2 <= 2
-==      igual               'a' == 'a'
-!=      diferente           'a' != 'b'
-"""
-maior = 2 > 1
-maior_ou_igual = 2 >= 2
-menor = 1 < 2
-menor_ou_igual = 2 <= 2
-igual = 'a' == 'a'
-diferente = 'a' != 'b'
-print('Olha meu print aqui')
+# Operadores lógicos
+# and (e) or (ou) not (não)
+# and - Todas as condições precisam ser
+# verdadeiras.
+# Se qualquer valor for considerado falso,
+# a expressão inteira será avaliada naquele valor
+# São considerados falsy (que vc já viu)
+# 0 0.0 '' False
+# Também existe o tipo None que é
+# usado para representar um não valor
+# entrada = input('[E]ntrar [S]air: ')
+# senha_digitada = input('Senha: ')
+
+# senha_permitida = '123456'
+
+# if entrada == 'E' and senha_digitada == senha_permitida:
+#     print('Entrar')
+# else:
+#     print('Sair')
+
+# Avaliação de curto circuito
+print(True and False and True)
+print(True and 0 and True)
