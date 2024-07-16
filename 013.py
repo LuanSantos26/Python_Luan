765e80f43cfcac5127e17542bde91f7f0bf7601c
Luan Feitosa Santos <luansantos26@outlook.com>
2024-07-16 20:45:55 -0300

    .

---
+++ 013.py
@@ -0,0 +1,7 @@
+# Operador lógico "not"
+# Usado para inverter expressões
+# não Verdadeiro = Falso
+# não Falso = Verdadeiro
+# senha = input('Senha: ')
+imprimir ( not  True )   # falso
+imprimir ( not  False)   # Verdadeir
