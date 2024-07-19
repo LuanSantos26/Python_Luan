2e35115fb5a3fbce14260673bf97cbdeab8e18d5
Luan Feitosa Santos <luansantos26@outlook.com>
2024-07-19 19:47:28 -0300

    Create exercicio.py

---
+++ exercicio.py
@@ -0,0 +1,14 @@
+nome =input('Digite seu nome: ')
+idade = input('Digite sua idade: ')
+if nome  and idade  :
+    print(f'seu nome é ',nome)
+    print(f'seu nome de tras para frente é ',nome [::1])
+    if ' ' in nome:
+        print('seu nome contem espaços.')
+    else:
+        print('Seu nome não contem espaços.')
+    print(f'seu nome contem {len (nome)} letras')
+    print(f'a primeira letra do seu nome é ',nome[0] )
+    print(f'e a ultima letra do seu nome é ',nome[-1])
+else:
+    print('Voce não digitou nada!')
