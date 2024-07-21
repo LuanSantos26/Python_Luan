43737d5b0338e61e30b7087b440d4337328cedd6
Luan Feitosa Santos <luansantos26@outlook.com>
2024-07-21 12:58:57 -0300

    Update try_except.py

--- try_except.py
+++ try_except.py
@@ -18,4 +18,4 @@ except:
 #     numero_float = float(numero_str)
 #     print(f'O dobro de {numero_str} é {numero_float * 2:.2f}')
 # else:
-#     print('Isso não é um número')
+#     print('Isso não é um número').
