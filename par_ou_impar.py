53146f6b58fb7dcac2e70bd6d13a44af828cbd3d
Luan Feitosa Santos <luansantos26@outlook.com>
2024-07-26 20:34:16 -0300

    Update par_ou_impar.py

--- par_ou_impar.py
+++ par_ou_impar.py
@@ -1,5 +1,6 @@
-numero = (input('Digite um numero inteiro para saber se é par ou impar: '))
+'''numero = (input('Digite um numero inteiro para saber se é par ou impar: '))
 numero_inteiro=int(numero)
+numero_str=str(numero)
 try:
     if numero_inteiro % 2 == 0:
         print(f'O numero {numero_inteiro} é par')
@@ -7,4 +8,20 @@ try:
         numero_inteiro % 2 == 1
         print(f'O numero {numero_inteiro} é impar')
 except ValueError:
-    print('Você não digitou nenhum numero')
+    if numero == str:
+        print ('Você não digitou nenhum numero')
+        '''
+def par_ou_impar():
+    try:
+        numero = input('Digite um número inteiro para saber se é par ou ímpar: ')
+        numero_inteiro = int(numero)
+
+        if numero_inteiro % 2 == 0:
+            print(f'O número {numero_inteiro} é par.')
+        else:
+            print(f'O número {numero_inteiro} é ímpar.')
+
+    except ValueError:
+        print('Você não digitou um número inteiro válido.')
+        return par_ou_impar()
+par_ou_impar()
