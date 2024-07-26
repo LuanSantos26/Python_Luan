ec7af984dd0612cdce8b9db656810b0a57d2253a
Luan Feitosa Santos <luansantos26@outlook.com>
2024-07-26 20:34:41 -0300

    Update exercicio2.py

--- exercicio2.py
+++ exercicio2.py
@@ -3,16 +3,20 @@ Faça um programa que peça ao usuário para digitar um número inteiro,
 informe se este número é par ou ímpar. Caso o usuário não digite um número
 inteiro, informe que não é um número inteiro.
 """
-numero = (input('Digite um numero inteiro para saber se é par ou impar: '))
-numero_inteiro=int(numero)
-try:
-    if numero_inteiro % 2 == 0:
-        print(f'O numero {numero_inteiro} é par')
-    else:
-        numero_inteiro % 2 == 1
-        print(f'O numero {numero_inteiro} é impar')
-except ValueError:
-    print('Você não digitou nenhum numero')
+def par_ou_impar():
+    try:
+        numero = input('Digite um número inteiro para saber se é par ou ímpar: ')
+        numero_inteiro = int(numero)
+
+        if numero_inteiro % 2 == 0:
+            print(f'O número {numero_inteiro} é par.')
+        else:
+            print(f'O número {numero_inteiro} é ímpar.')
+
+    except ValueError:
+        print('Você não digitou um número inteiro válido.')
+        return par_ou_impar()
+par_ou_impar()
 
 """
 Faça um programa que pergunte a hora ao usuário e, baseando-se no horário 
