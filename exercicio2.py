dabfd6d05bca43251d4d586dadcf2efe41060cd4
Luan Feitosa Santos <luansantos26@outlook.com>
2024-07-26 19:35:10 -0300

    Update exercicio2.py

--- exercicio2.py
+++ exercicio2.py
@@ -14,4 +14,4 @@ Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
 Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou 
 menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva 
 "Seu nome é normal"; maior que 6 escreva "Seu nome é muito grande". 
-"""
+."""
