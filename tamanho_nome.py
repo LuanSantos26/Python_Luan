30c2892bb1a02677135101db81bd589a1725f28a
Luan Feitosa Santos <luansantos26@outlook.com>
2024-07-26 21:48:05 -0300

    Update tamanho_nome.py

--- tamanho_nome.py
+++ tamanho_nome.py
@@ -1,3 +1,4 @@
+'''
 def tamanho_nome():
     primeiro_nome = input("Digite seu primeiro nome: ")
 
@@ -9,3 +10,16 @@ def tamanho_nome():
         print("Seu nome é muito grande.")
 
 tamanho_nome()
+'''
+nome = input('Digite seu nome: ')
+tamanho_nome = len(nome)
+
+if tamanho_nome > 1:
+    if tamanho_nome <= 4:
+        print('Seu nome é curto')
+    elif tamanho_nome >= 5 and tamanho_nome <= 6:
+        print('Seu nome é normal')
+    else:
+        print('Seu nome é muito grande')
+else:
+    print('Digite mais de uma letra.')
