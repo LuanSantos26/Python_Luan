6ea1491f768bbcde6d333964bfb69839b4f95575
Luan Feitosa Santos <luansantos26@outlook.com>
2024-07-26 21:39:35 -0300

    Update horas_agora.py

--- horas_agora.py
+++ horas_agora.py
@@ -1,4 +1,4 @@
-def horas_agora():
+'''def horas_agora():
     try:
         horas = input('Digite que horas são agora para saber o periodo do dia: ')
         horas_inteiro = int(horas)
@@ -13,3 +13,20 @@ def horas_agora():
         print('Voce não digitou as horas e sim colocou letras ou outros caracteres.')
         return horas_agora()
 horas_agora()
+   '''
+
+entrada = input('Digite a hora em números inteiros: ')
+
+try:
+    hora = int(entrada)
+
+    if hora >= 0 and hora <= 11:
+        print('Bom dia')
+    elif hora >= 12 and hora <= 17:
+        print('Bom tarde')
+    elif hora >= 18 and hora <= 23:
+        print('Bom noite')
+    else:
+        print('Não conheço essa hora')
+except:
+    print('Por favor, digite apenas números inteiros')
