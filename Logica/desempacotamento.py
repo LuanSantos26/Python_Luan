eee719893a77f99d55417124877b77ce8f4b6c8f
Luan Feitosa Santos <luansantos26@outlook.com>
2024-09-23 19:46:06 -0300

    Create desempacotamento.py

---
+++ Logica/desempacotamento.py
@@ -0,0 +1,8 @@
+"""
+Introdução ao desempacotamento + tuples (tuplas) 
+"""
+
+#nomes = ['Luan', 'Barto', 'Gera'] 
+# *_ = serve para empacotar os dados na variavel, geralmente é para variaveis que não serão utilizadas
+_, nome2, *_ = ['Luan', 'Barto', 'Gera', 'Cassia']
+print(nome2)
