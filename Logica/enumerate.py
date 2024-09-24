80c26a5a1191c5a6f2df28fbbc8b0d20adba40f3
Luan Feitosa Santos <luansantos26@outlook.com>
2024-09-24 19:53:29 -0300

    Update enumerate.py

--- Logica/enumerate.py
+++ Logica/enumerate.py
@@ -5,13 +5,23 @@ enumerate - enumera iteráveis (indices) = ele cria uma ordem de numeros na sua 
 lista = ['Luan', 'Barto', 'Geraldina']
 lista.append('Cassia')
 
-lista_enumerada = tuple(enumerate(lista))
+#lista_enumerada = tuple(enumerate(lista, start=20))
 
-print(lista_enumerada)
+#print(lista_enumerada)
 
-#for item in enumerate(lista):
-    #print(item)
+# for item in enumerate(lista):
+#     indice, nome = item
+#     print(indice, nome)
 
 
 
 #print(next(lista_enumerada))
+
+for tupla_enumerada in enumerate(lista):
+    print('For da tupla: ')
+    for valor in tupla_enumerada:
+      print(f'\t{valor}')
+
+# for item in enumerate(lista):
+#     for valor in item:
+#       print(valor)
