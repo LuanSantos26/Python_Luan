0c6b68380faa8218013901f0c0dab591f966db4c
Luan Feitosa Santos <luansantos26@outlook.com>
2024-09-24 20:21:32 -0300

    lista de mercado

--- Logica/enumerate.py
+++ Logica/enumerate.py
@@ -7,21 +7,21 @@ lista.append('Cassia')
 
 #lista_enumerada = tuple(enumerate(lista, start=20))
 
-#print(lista_enumerada)
+for indice, nome in enumerate(lista):
+    print(indice, nome)
 
 # for item in enumerate(lista):
 #     indice, nome = item
 #     print(indice, nome)
 
 
-
-#print(next(lista_enumerada))
-
-for tupla_enumerada in enumerate(lista):
-    print('For da tupla: ')
-    for valor in tupla_enumerada:
-      print(f'\t{valor}')
+# for tupla_enumerada in enumerate(lista):
+#     print('For da tupla: ')
+#     for valor in tupla_enumerada:
+#       print(f'\t{valor}')
 
 # for item in enumerate(lista):
 #     for valor in item:
 #       print(valor)
+
+#print(next(lista_enumerada))
