07dcd604599410e408a2312af7e59e6e84d76c62
Luan Feitosa Santos <luansantos26@outlook.com>
2025-02-05 19:47:46 -0300

    .

--- Logica/exercicio_cpf_part1.py
+++ Logica/exercicio_cpf_part1.py
@@ -26,11 +26,12 @@ O primeiro dígito do CPF é 7
 
 cpf = '74682489070'
 nove_digitos = cpf[:9]
-contado_regressivo = 10
+contado_regressivo_1 = 10
 
-resultado = 0
-for digito in nove_digitos:
-    resultado += int(digito) * contado_regressivo
-    contado_regressivo -= 1
-digito -= ((resultado * 10) % 11)
-
+resultado_digito_1 = 0
+for digito_1 in nove_digitos:
+    resultado_digito_1 += int(digito_1) * contado_regressivo_1
+    contado_regressivo_1 -= 1
+digito_1 = (resultado_digito_1 * 10) % 11
+digito_1 = digito_1 if digito_1 <=9 else 0
+print(digito_1)
