48303041a6206fe97ce7b42327e2ca8459526e1f
Luan Feitosa Santos <luansantos26@outlook.com>
2025-02-11 21:31:10 -0300

    escopo de funções

---
+++ Logica/func_escopo.py
@@ -0,0 +1,7 @@
+'''
+Escopo de funções em Python
+Escopo significa o local onde aquele codigo pode atingir.
+Existe o escopo global e local.
+O escopo global é o escopo onde todo o codigo é alcançavel.
+O escopo local é o escopo onde apenas nomes do mesmo local podem ser alcançados.
+'''
