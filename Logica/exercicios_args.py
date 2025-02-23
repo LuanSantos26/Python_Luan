8d09df5824f5258c8e15131def8515993fecb4a1
Luan Feitosa Santos <luansantos26@outlook.com>
2025-02-23 13:59:09 -0300

    .

--- Logica/exercicios_args.py
+++ Logica/exercicios_args.py
@@ -2,9 +2,9 @@
 def impar_ou_par ():
     input_numero = int(input("Digite um número: "))
     if input_numero % 2 == 0:
-        print("O número é par")
+        print(f"O número {input_numero} é par")
     else:
-        print("O número é ímpar")   
+        print(f"O número {input_numero} é ímpar")   
 impar_ou_par()       
 
 # Crie uam função que multiplica todos os argumentos não comeados recebidos
