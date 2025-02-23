327fdcbbfd671589fb6aa841acf0c4bfa80ba1fc
Luan Feitosa Santos <luansantos26@outlook.com>
2025-02-23 12:33:50 -0300

    args exercism

---
+++ Logica/exercicios_args.py
@@ -0,0 +1,7 @@
+def impar_ou_par ():
+    input_numero = int(input("Digite um número: "))
+    if input_numero % 2 == 0:
+        print("O número é par")
+    else:
+        print("O número é ímpar")   
+impar_ou_par()       
